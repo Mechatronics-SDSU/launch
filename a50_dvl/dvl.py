474bd19c3ab205b77fecd55ead6a56f379b0b26e
joelofrese <csommerfield3597@sdsu.edu>
2024-06-18 23:08:33 -0700

    Fix Errors and Adapt For Server

--- a50_dvl/dvl.py
+++ a50_dvl/dvl.py
@@ -3,21 +3,20 @@
 import socket
 import json
 
-# Define constants
-TCP_IP = '192.168.194.95'  # IP address of the A50 sensor
-TCP_PORT = 16171  # Port number the A50 sensor is listening on
-BUFFER_SIZE = 1024  # Size of the receive buffer
+
+TCP_IP = '192.168.194.95' 
+TCP_PORT = 16171 
+BUFFER_SIZE = 1024
 
 class DVL:
     def __init__(self):
-        self.sock = None  # Initialize socket to None
-        self.serv_addr = ("localhost", TCP_PORT)  # Server address (IP and port)
-        self.buffer = bytearray(BUFFER_SIZE)  # Create a buffer to store received data
+        self.serv_addr = (TCP_IP, TCP_PORT)
+        self.sock = self.connectToSocket()
+        self.buffer = bytearray(BUFFER_SIZE) 
         self.resetDeadReckoning()
 
     def resetDeadReckoning(self):
         try:
-            # Create a socket, connect to the server, send reset command, and close the socket
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect(self.serv_addr)
             json_command = {"command": "reset_dead_reckoning"}
@@ -28,13 +27,12 @@ class DVL:
 
     def connectToSocket(self):
         try:
-            # Create a socket and connect to the server
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect(self.serv_addr)
-            return sock  # Return the connected socket
+            return sock 
         except Exception as e:
             print("Failed to connect to socket:", e)
-            return None  # Return None if connection fails
+            return None 
 
     def parseJson(self, json_dict):
         try:    
@@ -44,40 +42,36 @@ class DVL:
             yaw = json_dict["yaw"]
             pitch = json_dict["pitch"]
             roll = json_dict["roll"]
-
             return [yaw, pitch, roll, x, y, z]
         except Exception as e:
             print("Failed to parse JSON:", e)
-            return []  # Return an empty list if parsing fails
+            return [] 
 
     def printData(self, a50_data):
-        print("Publishing State Data:")
-        print("Yaw:", a50_data[0], "Pitch:", a50_data[1], "Roll:", a50_data[2])
-        print("X:", a50_data[3], "Y:", a50_data[4], "Z:", a50_data[5])
+        print("yaw:", a50_data[0], "pitch:", a50_data[1], "roll:", a50_data[2])
+        print("x:", a50_data[3], "y:", a50_data[4], "z:", a50_data[5])
 
     def recieveData(self):
-        try:
-            # Receive data from the socket, parse JSON, and publish data
-            bytesRead = self.sock.recv(BUFFER_SIZE)
-            json_stream = bytesRead.decode()
-            json_dict = json.loads(json_stream)
-            a50_data = self.parseJson(json_dict)
-            self.printData(a50_data)
-            return a50_data
-        except Exception as e:
-            print("Error in getting A50 data:", e)
-            self.sock.close()
-            self.sock = self.connectToSocket()  # Reconnect if an error occurs
-            return None
+        dvl_data = None
+        while dvl_data == None:
+            try:
+                bytesRead = self.sock.recv(BUFFER_SIZE)
+                if len(bytesRead) < 600:
+                    json_dict = json.loads(bytesRead)
+                    a50_data = self.parseJson(json_dict)
+                    self.printData(a50_data)
+                    dvl_data = a50_data
+            except Exception as e:
+                print("Error in getting A50 data:", e)
+                self.sock.close()
+                self.sock = self.connectToSocket() 
+        return dvl_data
 
     def run(self):
-        # Connect to the socket and continuously receive data
-        self.sock = self.connectToSocket()
         if self.sock:
             while True:
-                self.recieveData()  # Receive and process A50 sensor data
+                self.recieveData() 
 
-# Run the node
 if __name__ == "__main__":
     a50_node = DVL()
     a50_node.run()

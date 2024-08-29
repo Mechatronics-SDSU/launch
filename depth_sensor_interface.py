7991759f2654104c5791cc4d2e0db7b3f825f906
kchan5071 <kchan5071@sdsu.edu>
2024-08-29 01:37:08 -0700

    added shared memory object

--- depth_sensor_interface.py
+++ depth_sensor_interface.py
@@ -2,25 +2,21 @@ from sensors.depth_sensor import DepthSensor
 from multiprocessing import Value
 
 class DepthSensorInterface:
-    def __init__(self, z):
-        self.z = z
+    def __init__(self, shared_memory_object):
         self.depth_sensor = DepthSensor()
+        self.shared_memory_object = shared_memory_object
 
     def update(self):
         depth = self.depth_sensor.recieve_data()
         if depth != None and len(depth) > 3:
             if"Depth" in depth:
                 print(float(depth[depth.find(" ") + 1:]))
-                self.z.value = float(depth[depth.find(" ") + 1:])
+                self.shared_memory_object.depth.value = float(depth[depth.find(" ") + 1:])
 
     def print_data(self):
-        print(self.z.value)
+        print(self.shared_memory_object.depth.value)
 
     def run_loop(self):
-        while True:
+        while self.shared_memory_object.running.value:
             self.update()
 
-if __name__ == "__main__":
-    depth_z = Value('d', 0.0)
-    depth_sensor_interface = DepthSensorInterface(z=depth_z)
-    depth_sensor_interface.run_loop()
